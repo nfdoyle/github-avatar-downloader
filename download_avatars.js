9421a0ce9c0b3f78a81d177439acc5c96ce0473e
nfdoyle <nickdoyle98@gmail.com>
2019-01-15 02:19:35 +0000

    added/protected gh keys

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
+var secret = require('./secrets');
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secret.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
